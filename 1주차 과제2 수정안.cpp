c0bc63d18b6299052e2b20cec2dc83b7f862996f
Leeminju329 <63380889+Leeminju329@users.noreply.github.com>
2020-04-18 15:59:44 +0900

    Add files via upload

---
+++ 1주차 과제2 수정안.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	float num1, num2;
+
+	cout << "First number>> ";
+	cin >> num1;
+
+	cout << "Second number>> ";
+	cin >> num2;
+
+	cout << "=========계산 결과==========\n";
+
+
+	cout << num1 << "+" << num2 << "=" << (num1 + num2) << '\n';
+	cout << num1 << "-" << num2 << "=" << (num1 - num2) << '\n';
+	cout << num1 << "*" << num2 << "=" << (num1 * num2) << '\n';
+	cout << num1 << "/" << num2 << "=" << (num1 / num2) << '\n';
+
+	
+
+	
+
+}
