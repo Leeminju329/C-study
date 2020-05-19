e1f1b8f788a9ecfc038799531a16e06faa024d56
Leeminju329 <63380889+Leeminju329@users.noreply.github.com>
2020-05-19 19:28:59 +0900

    Add files via upload

---
+++ 스터디 실습(별찍기).cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+	int i, k;
+	for (i = 0; i < 5; i++) 
+	{
+		for (k = 0; k < i+1; k++) 
+		{
+				cout << "*";
+
+		}
+
+		cout << endl;
+	}
+
+}
